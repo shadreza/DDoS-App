ddba70af515e943590e57267e56e9428c1beccf5
shadreza <shadreza100@gmail.com>
2023-01-30 17:06:17 +0600

    moved the reducer functionality to another component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
-import { RootState } from './redux/store';
-import { useSelector, useDispatch } from 'react-redux';
-import { incrementTemp, decrementTemp } from './redux/features/temp';
+import Temp from './components/redux-functions/Temp';
 
 function App() {
 
-  // the state.[name] -> [name] comes from the store.ts as we are using the object property
-  const {tmpVariable} = useSelector((state: RootState) => state.tmpStore)
-  const dispatch = useDispatch();
-
   return (
     <div className="App">
-      <button
-        aria-label="Increment value"
-        onClick={() => dispatch(incrementTemp())}
-      >
-        Increment
-      </button>
-      <span>{tmpVariable}</span>
-      <button
-        aria-label="Decrement value"
-        onClick={() => dispatch(decrementTemp())}
-      >
-        Decrement
-      </button>
+      <Temp/>
     </div>
   );
 }
